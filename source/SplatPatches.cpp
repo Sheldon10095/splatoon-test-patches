bd7f7131ef2ea6e13beae473f5ddfc2a7d5ae30a
Sheldon10095 <18726387+Sheldon10095@users.noreply.github.com>
2023-01-19 19:13:00 -0500

    Some cleanup, and add small lib for easier code patching

--- source/SplatPatches.cpp
+++ source/SplatPatches.cpp
@@ -1,5 +1,6 @@
 #include "SplatPatches.hpp"
 #include "CodePatchUtils.hpp"
+#include "creamer/patch/code_patcher.hpp"
 
 namespace sead
 {
@@ -118,135 +119,47 @@ namespace Splatoon
     static uintptr_t dataAddr = 0;
 
 
+    static sead::Color4f rainbowColors[7] = {
+        // Red
+        { 1.0f, 0.0f, 0.0f, 1.0f },
+        // Orange
+        { 1.0f, 0.5f, 0.0f, 1.0f },
+        // Yellow
+        { 1.0f, 1.0f, 0.0f, 1.0f },
+        // Green
+        { 0.0f, 1.0f, 0.0f, 1.0f },
+        // Blue
+        { 0.0f, 0.0f, 1.0f, 1.0f },
+        // Indigo
+        { 0.5f, 0.0f, 1.0f, 1.0f },
+        // Violet
+        { 0.75f, 0.0f, 1.0f, 1.0f }
+    };
+
     // void MyTestFunc()
     void MyTestFunc(sead::Viewport const* pViewport)
     {
-        // static bool first = true;
-        // if (first) {
-        //     WHBLogPrintf("splatoon_test_patches: MyTestFunc() called! First time.");
-        //     first = false;
-        // }
-
-        // // static int i = 60;
-        // // if (i > 0) {
-        // //     i--;
-        // // } else {
-        // //     WHBLogPrintf("splatoon_test_patches: MyTestFunc() called! Heartbeat Message. (Expected every 60 frames)");
-        // //     i = 60;
-        // // }
+        static bool first = true;
+        if (first) {
+            WHBLogPrintf("splatoon_test_patches: MyTestFunc() called! First time.");
+            first = false;
+        }
 
         beginDrawImm(mtx34Ident, mtx44Ident, 4);
-
-        drawBoundBoxImm(box1, color1, 1.0f);
+        // drawBoundBoxImm(box1, color1, 1.0f);
         // drawBoundBoxImm(box2, color2, 1.0f);
+
+        // Make a rainbow box
+        for (int i = 0; i < 7; i++) {
+            sead::BoundBox3<float> box = {
+                { -0.97f + (i * 0.01f), -0.97f + (i * 0.01f), 0.0f },
+                { 0.97f - (i * 0.01f), 0.97f - (i * 0.01f), 0.0f }
+            };
+            drawBoundBoxImm(box, rainbowColors[i], 1.0f);
+        }
+        
         // // drawBoundBoxImm(box2, color2, 15.0f);
-
-        // // drawPointImm({ 0.0f, 0.0f, 0.0f }, color2, 30.0f);
-
-        // // // WHBLogPrintf("Attempting to construct TextWriter... (pViewport = %p)", pViewport);
-        // sead::TextWriter writer = { 0 };
-        // auto test = sead_TextWriter__ct_test(&writer, pViewport);
-
-        // // sead_TextWriter_printf(test, "TEST MESSAGE 1");
-        // // test->_0[0x48] = 0x0;
-        // // sead_TextWriter_printf(test, "TEST MESSAGE 2");
-        // // test->_0[0x48] = 0x1;
-        // // sead_TextWriter_printf(test, "TEST MESSAGE 3");
-        // // test->_0[0x48] = 0x0;
-        // // sead_TextWriter_printf(test, "TEST MESSAGE 4");
-
-        // sead_TextWriter_printf(&writer, "TEST MESSAGE 1");
-
-        // writer.mVector18.y = 1.8f;
-        // writer.mVector18.x = 1.8f;
-        // // writer.mVector10.y = 0.1f;
-        // // writer.mVector10.x = 0.1f;
-        // writer.mVector10.x = -160.0f;
-        // writer.mVector10.y = -300.0f;
-        // writer.mColor = color1;
-
-        // sead_TextWriter_printf(&writer, "TEST MESSAGE 2");
-
-
-        // if (test) {
-        //     // WHBLogPrintf("TextWriter constructed successfully! (maybe???)");
-        //     sead_TextWriter_printf(test, "Hello World!");
-        //     // WHBLogPrintf("TextWriter printed successfully!");
-        // } else {
-        //     // WHBLogPrintf("TextWriter construction failed!");
-        // }
-
-        // static sead::TextWriter *writer = nullptr;
-        // if (!writer) {
-        //     // writer = (sead::TextWriter*)malloc(sizeof(sead::TextWriter));
-        //     // writer = (sead::TextWriter*)malloc(80);
-        //     writer = sead_TextWriter__ct(writer);
-        //     WHBLogPrintf("splatoon_test_patches:    Created TextWriter.");
-        // }
-
-        // if (writer) {
-        //     WHBLogPrintf("splatoon_test_patches:    Writing to TextWriter...");
-        //     sead_TextWriter_printf(writer, "Hello World!");
-        // }
-
-        // auto writer = sead_TextWriter__ct_test(writer, 0x104141f4);
-        // if (writer) {
-        //     WHBLogPrintf("splatoon_test_patches:    Writing to TextWriter...");
-        //     sead_TextWriter_printf(writer, "Hello World!");
-        // } else {
-        //     WHBLogPrintf("splatoon_test_patches:    Failed to create TextWriter.");
-        // }
-
-        // static sead::TextWriter *writer = nullptr;
-        // if (!writer) {
-        //     writer = sead_TextWriter__ct_test(writer, 0x104141f4);
-        //     WHBLogPrintf("splatoon_test_patches:    Created TextWriter.");
-        // } else {
-        //     sead_TextWriter_printf(writer, "Hello World!");
-        // }
-
-        // static sead::TextWriter writer = { 0 };
-        // auto test_res = sead_TextWriter__ct_test(&writer, 0x104141f4);
-        // WHBLogPrintf("test_res = %p", test_res);
-        // // WHBLogPrintf("&test_res = %p", &test_res);
-        // // WHBLogPrintf("*test_res = %p", *test_res);
-        // WHBLogPrintf("&writer = %p", &writer);
-        // // WHBLogPrintf("writer = %p", writer);
-        // if (test_res) {
-        //     WHBLogPrintf("splatoon_test_patches:    Writing to TextWriter...");
-        //     sead_TextWriter_printf(test_res, "Hello World!");
-        // }
-
-        // static sead::TextWriter writer = { 0 };
-        // // int a2 = 0x104141f4 + 0x503000;
-        // int a2 = dataAddr + 0x4141f4;
-        // // int a2 = dataAddr + 0x41421c;
-        
-        // WHBLogPrintf("Before Constructing TextWriter:");
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[0], writer._0[1], writer._0[2], writer._0[3]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[4], writer._0[5], writer._0[6], writer._0[7]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[8], writer._0[9], writer._0[10], writer._0[11]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[12], writer._0[13], writer._0[14], writer._0[15]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[16], writer._0[17], writer._0[18], writer._0[19]);
-
-        // auto test_res = sead_TextWriter__ct_test(&writer, a2);
-
-        // WHBLogPrintf("After Constructing TextWriter:");
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[0], writer._0[1], writer._0[2], writer._0[3]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[4], writer._0[5], writer._0[6], writer._0[7]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[8], writer._0[9], writer._0[10], writer._0[11]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[12], writer._0[13], writer._0[14], writer._0[15]);
-        // WHBLogPrintf("%08x %08x %08x %08x", writer._0[16], writer._0[17], writer._0[18], writer._0[19]);
-
-        // if (test_res) {
-        //     WHBLogPrintf("splatoon_test_patches:    Writing to TextWriter...");
-        //     sead_TextWriter_printf(test_res, "Hello World!");
-        // }
-
-
-        // WHBLogPrintf("splatoon_test_patches:    writer = %p", writer);
-        
-        // sead_TextWriter_printf(writer, "Did you just use a nullptr as the this pointer?");
+        // drawPointImm({ 0.0f, 0.0f, 0.0f }, color2, 30.0f);
     }
 
     void MyTestFunc2(sead::TextWriter *writer)
@@ -260,17 +173,23 @@ namespace Splatoon
 
         writer->mScale.x = 3.0f;
         writer->mScale.y = 3.0f;
-
         sead_TextWriter_printf(writer, "LARGE TEXT\n");
 
         writer->mPos.x = -160.0f;
         writer->mPos.y = -300.0f;
         writer->mScale.x = 1.8f;
         writer->mScale.y = 1.8f;
-
         sead_TextWriter_printf(writer, "Made by Sheldon\n");
     }
 
+    u16 HIWORD(u32 value)
+    {
+        return (u16)(value >> 16);
+    }
+    u16 LOWORD(u32 value)
+    {
+        return (u16)(value & 0xFFFF);
+    }
 
     void ApplyPatches()
     {
@@ -284,17 +203,6 @@ namespace Splatoon
         // ~~~ Begin Patches ~~~
 
         // // Address of 'gsys::SystemTask::invokeDrawTV_' is 0x02AD7160
-        // u32 addr_func_gsys__SystemTask__invokeDrawTV_ = gambit_rpx->textAddr + 0xAD7160;
-        // // Patch func + 0x144 to 0x60000000 (nop)
-        // u32 patch1 = addr_func_gsys__SystemTask__invokeDrawTV_ + 0x144; // 0x02AD72A4
-        // PatchInstruction((u32*)patch1, 0x41820068, 0x60000000); // beq + 0x68 -> nop
-        // 
-        // // Address of 'gsys::SystemTask::invokeDrawTV_' is 0x02AD7160
-        // uintptr_t addr_func_gsys__SystemTask__invokeDrawTV_ = gambit_rpx->textAddr + 0xAD7160;
-        // 
-        // // Patch func + 0x144 to 0x60000000 (nop)
-        // uintptr_t patch1 = addr_func_gsys__SystemTask__invokeDrawTV_ + 0x144; // 0x02AD72A4
-        // PatchInstruction((uintptr_t *)patch1, 0x41820068, 0x60000000);        // beq + 0x68 -> nop
 
         WHBLogPrintf("Gambit.rpx->textAddr: %08x", gambit_rpx->textAddr);
         WHBLogPrintf("Gambit.rpx->dataAddr: %08x", gambit_rpx->dataAddr);
@@ -305,64 +213,60 @@ namespace Splatoon
 
         uintptr_t base = gambit_rpx->textAddr;
         uintptr_t func = base + 0xAD7160; /* gsys::SystemTask::invokeDrawTV_ */
-        // UTL::WriteCode(func + 0x144, UTL::inst::Nop); // .text:02AD72A4         nop
-        // UTL::WriteCode(func + 0x148, UTL::inst::BranchLink())
+        uintptr_t func2 = base + 0x89dcd8;
 
-        // Attempt to call test function
-        // UTL::WriteCode(func + 0x144, UTL::inst::BranchLink(((uintptr_t)&MyTestFunc) - (func + 0x144 + 4)));
-        // auto MyTestFunc_addr = OSEffectiveToPhysical((uintptr_t)&MyTestFunc);
-        // UTL::WriteCode(func + 0x144, UTL::inst::BranchLink(MyTestFunc_addr - (func + 0x144)));
-        // WHBLogPrintf("SPT: [DEBUG] MyTestFunc() address: %p", &MyTestFunc);
-        // WHBLogPrintf("SPT: [DEBUG] MyTestFunc() Physical Address: %p", OSEffectiveToPhysical((uint32_t)&MyTestFunc));
-        // WHBLogPrintf("SPT: [DEBUG] func + 0x144: %p", func + 0x144);
-        // WHBLogPrintf("SPT: [DEBUG] func + 0x144 physical: %p", OSEffectiveToPhysical(func + 0x144));
-        // WHBLogPrintf("SPT: [DEBUG] MyTestFunc_addr - (func + 0x144): %p", MyTestFunc_addr - (func + 0x144));
-        // WHBLogPrintf("SPT: [DEBUG] %08x", ((uintptr_t)&MyTestFunc) - (func + 0x144));
-        // WHBLogPrintf("SPT: [DEBUG] ^ as PPC Hex Inst: %08x", UTL::inst::BranchLink(((uintptr_t)&MyTestFunc) - (func + 0x144)));
+        crm::WriteCode(func + 0x144, crm::inst::AddImmediate(crm::Register::r3, crm::Register::r1, 0x30));
+        crm::WriteCode(func + 0x148, crm::inst::LoadImmediateShifted(crm::Register::r11, HIWORD((uintptr_t)&MyTestFunc)));
+        crm::WriteCode(func + 0x14C, crm::inst::OrImmediate(crm::Register::r11, crm::Register::r11, LOWORD((uintptr_t)&MyTestFunc)));
+        crm::WriteCode(func + 0x150, crm::inst::MoveToCountRegister(crm::Register::r11));
+        crm::WriteCode(func + 0x154, crm::inst::BranchCountLink());
+        crm::WriteCode(func + 0x158, crm::inst::Branch(0x1AC - 0x158));
 
+        crm::WriteCode(func2 + 0x4c, crm::inst::NoOperation());
+        crm::WriteCode(func2 + 0x64, crm::inst::LoadImmediateShifted(crm::Register::r11, HIWORD((uintptr_t)&MyTestFunc2)));
+        crm::WriteCode(func2 + 0x68, crm::inst::OrImmediate(crm::Register::r11, crm::Register::r11, LOWORD((uintptr_t)&MyTestFunc2)));
+        crm::WriteCode(func2 + 0x6c, crm::inst::MoveToCountRegister(crm::Register::r11));
+        crm::WriteCode(func2 + 0x70, crm::inst::BranchCountLink());
+        crm::WriteCode(func2 + 0x74, crm::inst::Branch(0x280 - 0x74));
+
+        /// OLD CODE
         // UTL::WriteCode(func + 0x144, 0x3d600000 | ((((uintptr_t)&MyTestFunc) >> 16) & 0x0000FFFF)); // lis r11, MyTestFunc@ha
         // UTL::WriteCode(func + 0x148, 0x616b0000 | (((uintptr_t)&MyTestFunc) & 0x0000FFFF));         // ori r11, r11, MyTestFunc@l
         // UTL::WriteCode(func + 0x14c, 0x7d6903a6); // mtspr, ctr, r11
         // UTL::WriteCode(func + 0x150, 0x4e800421); // bctrl
         // // Jump forward a bit to skip the built in debug draw code. (Jump to func + 0x1AC)
         // UTL::WriteCode(func + 0x154, UTL::inst::Branch(0x1AC - 0x154)); // b 0x58
-
         
-        // Slight revision to above code
-        UTL::WriteCode(func + 0x144, 0x38610030);                                                   // addi r3, r1, 0x30
-        UTL::WriteCode(func + 0x148, 0x3d600000 | ((((uintptr_t)&MyTestFunc) >> 16) & 0x0000FFFF)); // lis r11, MyTestFunc@ha
-        UTL::WriteCode(func + 0x14C, 0x616b0000 | (((uintptr_t)&MyTestFunc) & 0x0000FFFF));         // ori r11, r11, MyTestFunc@l
-        UTL::WriteCode(func + 0x150, 0x7d6903a6);                                                   // mtspr, ctr, r11
-        UTL::WriteCode(func + 0x154, 0x4e800421);                                                   // bctrl
-        // Jump forward a bit to skip the built in debug draw code. (Jump to func + 0x1AC)
-        UTL::WriteCode(func + 0x158, UTL::inst::Branch(0x1AC - 0x158)); // b 0x54
-        
+        // // Slight revision to above code
+        // UTL::WriteCode(func + 0x144, 0x38610030);                                                   // addi r3, r1, 0x30
+        // UTL::WriteCode(func + 0x148, 0x3d600000 | ((((uintptr_t)&MyTestFunc) >> 16) & 0x0000FFFF)); // lis r11, MyTestFunc@ha
+        // UTL::WriteCode(func + 0x14C, 0x616b0000 | (((uintptr_t)&MyTestFunc) & 0x0000FFFF));         // ori r11, r11, MyTestFunc@l
+        // UTL::WriteCode(func + 0x150, 0x7d6903a6);                                                   // mtspr, ctr, r11      (also: mtctr r11)
+        // UTL::WriteCode(func + 0x154, 0x4e800421);                                                   // bctrl
+        // // Jump forward a bit to skip the built in debug draw code. (Jump to func + 0x1AC)
+        // UTL::WriteCode(func + 0x158, UTL::inst::Branch(0x1AC - 0x158)); // b 0x54
+
+        // // Different branch location test
+        // uintptr_t func2 = base + 0x89dcd8;
+        // UTL::WriteCode(func2 + 0x4c, UTL::inst::Nop);
+        // UTL::WriteCode(func2 + 0x64, 0x3d600000 | ((((uintptr_t)&MyTestFunc2) >> 16) & 0x0000FFFF)); // lis r11, MyTestFunc2@ha
+        // UTL::WriteCode(func2 + 0x68, 0x616b0000 | (((uintptr_t)&MyTestFunc2) & 0x0000FFFF));         // ori r11, r11, MyTestFunc2@l
+        // UTL::WriteCode(func2 + 0x6c, 0x7d6903a6);                                                    // mtspr, ctr, r11
+        // UTL::WriteCode(func2 + 0x70, 0x4e800421);                                                    // bctrl
+        // // Jump to end of function for cleanup
+        // UTL::WriteCode(func2 + 0x74, UTL::inst::Branch(0x280 - 0x74)); // b 0x20C
+        /// END OLD CODE
 
 
-        // Different branch location test
-        uintptr_t func2 = base + 0x89dcd8;
-        UTL::WriteCode(func2 + 0x4c, UTL::inst::Nop);
-        UTL::WriteCode(func2 + 0x64, 0x3d600000 | ((((uintptr_t)&MyTestFunc2) >> 16) & 0x0000FFFF)); // lis r11, MyTestFunc2@ha
-        UTL::WriteCode(func2 + 0x68, 0x616b0000 | (((uintptr_t)&MyTestFunc2) & 0x0000FFFF));         // ori r11, r11, MyTestFunc2@l
-        UTL::WriteCode(func2 + 0x6c, 0x7d6903a6);                                                    // mtspr, ctr, r11
-        UTL::WriteCode(func2 + 0x70, 0x4e800421);                                                    // bctrl
-        // Jump to end of function for cleanup
-        UTL::WriteCode(func2 + 0x74, UTL::inst::Branch(0x280 - 0x74)); // b 0x20C
-
-
-
-        drawBoundBoxImm = (void (*)(sead::BoundBox3<float> const &box, sead::Color4f const &color, float a3))(base + 0xA11158);
+        // ~~~~~~~~~~~~~~
+         
         beginDrawImm = (void (*)(sead::Matrix34<float> const& mtx34, sead::Matrix44<float> const& mtx44, int a3))(base + 0xA0ED08);
-
         drawPointImm = (void (*)(sead::Vector3<float> const& pos, sead::Color4f const& color, float a3))(base + 0xA1100C);
+        drawBoundBoxImm = (void (*)(sead::BoundBox3<float> const &box, sead::Color4f const &color, float a3))(base + 0xA11158);
 
-
-
-
-        sead_TextWriter__ct = (sead::TextWriter * (*)(sead::TextWriter * _this))(base + 0x8CC3B4);
         sead_TextWriter_printf = (void (*)(sead::TextWriter * _this, const char * format, ...))(base + 0x8CC420);
-
+        // sead_TextWriter__ct = (sead::TextWriter * (*)(sead::TextWriter * _this))(base + 0x8CC3B4);
         // sead_TextWriter__ct_test = (sead::TextWriter * (*)(sead::TextWriter * _this, int))(base + 0x8CC3B4);
-        sead_TextWriter__ct_test = (sead::TextWriter * (*)(sead::TextWriter * _this, sead::Viewport const *))(base + 0x8CC3B4);
+        // sead_TextWriter__ct_test = (sead::TextWriter * (*)(sead::TextWriter * _this, sead::Viewport const *))(base + 0x8CC3B4);
     }
 }
---
+++ source/creamer/patch/code_patcher.hpp
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <coreinit/cache.h>
+#include "types.h"
+
+#define PACKED __attribute__((__packed__))
+
+namespace crm
+{
+    using Instruction = uint32_t;
+
+    enum Register : int
+    {
+        r0 = 0,
+        r1 = 1,
+        r2 = 2,
+        r3 = 3,
+        r4 = 4,
+        r5 = 5,
+        r6 = 6,
+        r7 = 7,
+        r8 = 8,
+        r9 = 9,
+        r10 = 10,
+        r11 = 11,
+        r12 = 12,
+        r13 = 13,
+        r14 = 14,
+        r15 = 15,
+        r16 = 16,
+        r17 = 17,
+        r18 = 18,
+        r19 = 19,
+        r20 = 20,
+        r21 = 21,
+        r22 = 22,
+        r23 = 23,
+        r24 = 24,
+        r25 = 25,
+        r26 = 26,
+        r27 = 27,
+        r28 = 28,
+        r29 = 29,
+        r30 = 30,
+        r31 = 31,
+    };
+
+    // https://arcb.csc.ncsu.edu/~mueller/cluster/ps3/SDK3.0/docs/arch/PPC_Vers202_Book1_public.pdf
+
+    namespace InstFmt
+    {
+        // 1.7.1
+        struct PACKED IForm
+        {
+            u8 opcd : 6;     // Opcode              | 0 - 6
+            u32 li : 30 - 6; // ???                 | 6 - 30
+            u8 aa : 30 - 29; // Absolute Address    | 30
+            u8 lk : 31 - 30; // Link                | 31
+
+            // Works for Wii U, but is not portable
+            // operator u32() const { return *reinterpret_cast<const u32 *>(this); }
+            
+            // Portable version
+            operator u32() const {
+                return (opcd << 26) | (li << 2) | (aa << 1) | lk;
+            }
+        };
+        static_assert(sizeof(IForm) == sizeof(u32));
+
+        // 1.7.2
+        struct PACKED BForm
+        {
+            u8 opcd : 6;      // Opcode       | 0 - 6
+            u8 bo : 11 - 6;   // ???          | 6 - 11
+            u8 bi : 16 - 11;  // ???          | 11 - 16
+            u16 bd : 30 - 16; // ???          | 16 - 30
+            u8 aa : 30 - 29;  // Absolute     | 30
+            u8 lk : 31 - 30;  // Link         | 31
+        };
+        static_assert(sizeof(BForm) == sizeof(u32));
+
+        // 1.7.4 ~ Type 2
+        struct PACKED DForm
+        {
+            u8 opcd : 6;          // Opcode       | 0 - 6
+            Register rt : 11 - 6; // ???          | 6 - 11
+            u8 ra : 16 - 11;      // ???          | 11 - 16     | Either a Register or 0
+            u16 si : 30 - 16;     // ???          | 16 - 30
+
+            // operator u32() const { return *reinterpret_cast<const u32 *>(this); }
+            operator u32() const {
+                return (opcd << 26) | (rt << 21) | (ra << 16) | (si << 0);
+            }
+        };
+        static_assert(sizeof(DForm) == sizeof(u32));
+
+        // 1.7.7 ~ Type 2
+        struct PACKED XLForm
+        {
+            // OPCD BO BI /// BH XO LK
+            u8 opcd : 6;      // Opcode       | 0 - 6
+            u8 bo : 11 - 6;   // ???          | 6 - 11
+            u8 bi : 16 - 11;  // ???          | 11 - 16
+            int : 19 - 16;    // ///          | 16 - 19
+            u8 bh : 21 - 19;  // ???          | 19 - 21
+            u16 xo : 31 - 21; // ???          | 21 - 31
+            u8 lk : 31 - 30;  // Link         | 31
+
+            // operator u32() const { return *reinterpret_cast<const u32 *>(this); }
+            constexpr operator u32() const {
+                return (opcd << 26) | (bo << 21) | (bi << 16) | (bh << 11) | (xo << 1) | lk;
+            }
+        };
+        static_assert(sizeof(XLForm) == sizeof(u32));
+
+        // 1.7.8 ~ Type 7
+        struct PACKED XFXForm
+        {
+            u8 opcd : 6;          // Opcode       | 0 - 6
+            Register rs : 11 - 6; // ???          | 6 - 11
+            u16 spr : 21 - 11;    // ???          | 11 - 21
+            u16 xo : 31 - 21;     // ???          | 21 - 31
+            int : 1;              // N/A          | 31
+
+            XFXForm(u8 opcd, Register rs, u16 spr, u16 xo) : opcd(opcd), rs(rs), /*spr(spr),*/ xo(xo)
+            {
+                // Swap SPR
+                spr &= 0x3FF;
+                this->spr = (spr << 5) | (spr >> 5);
+            }
+
+            // Ctor is used to swap SPR before casting
+            // operator u32() const { return *reinterpret_cast<const u32 *>(this); }
+            constexpr operator u32() const {
+                return (opcd << 26) | (rs << 21) | (spr << 11) | (xo << 1);
+            }
+        };
+        static_assert(sizeof(XFXForm) == sizeof(u32));
+    }
+
+    namespace inst
+    {
+        static Instruction AddImmediate(Register rt, Register ra, u16 si) { return InstFmt::DForm(14, rt, ra, si); }        // addi
+        static Instruction AddImmediate(Register rt, u8 ra, u16 si) { return InstFmt::DForm(14, rt, ra, si); }              // addi
+        static Instruction AddImmediateShifted(Register rt, Register ra, u16 si) { return InstFmt::DForm(15, rt, ra, si); } // addis
+        static Instruction AddImmediateShifted(Register rt, u8 ra, u16 si) { return InstFmt::DForm(15, rt, ra, si); }       // addis
+
+        static Instruction Branch(u32 offset) { return InstFmt::IForm(18, (offset & 0x3FFFFFC) >> 2, 0, 0); }             // b
+        static Instruction BranchLink(u32 offset) { return InstFmt::IForm(18, (offset & 0x3FFFFFC) >> 2, 0, 1); }         // bl
+        static Instruction BranchAbsolute(u32 offset) { return InstFmt::IForm(18, (offset & 0x3FFFFFC) >> 2, 1, 0); }     // ba
+        static Instruction BranchLinkAbsolute(u32 offset) { return InstFmt::IForm(18, (offset & 0x3FFFFFC) >> 2, 1, 1); } // bla
+
+        static constexpr Instruction BranchCount() { return InstFmt::XLForm(19, 20, 0, 0, 528, 0); }     // bctr
+        static constexpr Instruction BranchCountLink() { return InstFmt::XLForm(19, 20, 0, 0, 528, 1); } // bctrl
+
+        static Instruction LoadImmediate(Register rt, u16 si) { return AddImmediate(rt, 0, si); }               // li
+        static Instruction LoadImmediateShifted(Register rt, u16 si) { return AddImmediateShifted(rt, 0, si); } // lis
+
+        static Instruction MoveToSpecialPurposeRegister(Register rs, u16 spr) { return InstFmt::XFXForm(31, rs, spr, 467); } // mtspr
+        static Instruction MoveToCountRegister(Register rs) { return MoveToSpecialPurposeRegister(rs, 9); }                  // mtctr
+
+        static Instruction OrImmediate(Register rt, Register ra, u16 si) { return InstFmt::DForm(24, rt, ra, si); }        // ori
+        static Instruction OrImmediate(Register rt, u8 ra, u16 si) { return InstFmt::DForm(24, rt, ra, si); }              // ori
+
+        static constinit Instruction Nop = 0x60000000; // nop
+        static Instruction NoOperation() { OrImmediate(crm::Register::r0, 0, 0); } // nop (alias, same as above)
+    }
+
+    // Code Writing Functions
+    template<typename T>
+    void Write(uintptr_t address, T value) {
+        /* Have kernel do the copy. */
+        KernelCopyData(
+            OSEffectiveToPhysical(address),
+            OSEffectiveToPhysical(reinterpret_cast<uintptr_t>(&value)),
+            sizeof(T)
+        );
+
+        /* Flush data cache. */
+        DCFlushRange(reinterpret_cast<void*>(address), sizeof(T));
+    }
+    
+    inline void WriteCode(uintptr_t address, Instruction value) {
+        /* Write data. */
+        Write(address, value);
+        
+        /* Flush instruction cache. */
+        ICInvalidateRange(reinterpret_cast<void*>(address), sizeof(Instruction));
+    }
+}
